51b1e2fd79477b8c97ea491141d8442d3dc18985
Zsuzsi <priskin.zsuzsanna@gmail.com>
2023-11-17 12:38:36 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EMPTY, Observable, Subject, of } from 'rxjs';
+import {debounceTime, distinctUntilChanged, switchMap, tap, catchError, retry} from 'rxjs/operators'
+import {HttpClient} from '@angular/common/http'
+
+type Pokemon = {
+  name: string,
+  spites: {
+    back_default: string,
+    front_default: string,
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokemon-search-rxjs';
+  inputValue : string = ''
+  private searchTerm$ = new Subject<string>();
+  searchResult$: Observable<any>;
+  errorMessage: string = ''
+
+  constructor(private http: HttpClient) {
+    this.searchResult$ = this.searchTerm$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(term => console.log('Input:', term)),
+      switchMap(term => this.apiCall(term)),
+      tap(result => this.inputValue = ''),
+      retry(3),
+      catchError(error => {
+        console.error('Error fetching data:', error);
+        this.errorMessage = 'An error occurred. Please try again.';
+        return of(null); // Return an observable to continue the stream
+        
+      })
+    );
+  }
+
+  onInputChange(): void {
+    this.searchTerm$.next(this.inputValue);
+  }
+
+  apiCall(term: string): Observable<any> {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${term}`;
+    return this.http.get(apiUrl);
+  }
 }
+
